589915a21cf6cdfd5936fe1f6fdb471060dd2274
jinwon93 <59594415+jinwon93@users.noreply.github.com>
2021-09-11 00:01:51 +0900

    Update async.js

--- async/async.js
+++ async/async.js
@@ -58,3 +58,19 @@ function delay(ms) {
     return Promise.race([getApple(), getBanana()]);
   }
   pickOnlyOne().then(console.log);
+
+//test
+let first = 10;
+let second = 20;
+let result = 0;
+
+function add(x, y) {
+    return x + y;
+}
+
+setTimeout(() => {
+    result = add(first, second);
+    console.log(result)
+}, 1000);
+
+first = 20;
